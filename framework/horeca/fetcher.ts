5914472e1d79bb013832b17c636a52e34ffaecc0
Yevhenii <Jimmydeanmaster@gmail.com>
2021-09-15 11:24:15 +0300

    initial

---
+++ framework/horeca/fetcher.ts
@@ -0,0 +1,46 @@
+import { FetcherError } from '@commerce/utils/errors'
+import type { Fetcher } from '@commerce/utils/types'
+
+async function getText(res: Response) {
+  try {
+    return (await res.text()) || res.statusText
+  } catch (error) {
+    return res.statusText
+  }
+}
+
+async function getError(res: Response) {
+  if (res.headers.get('Content-Type')?.includes('application/json')) {
+    const data = await res.json()
+    return new FetcherError({ errors: data.errors, status: res.status })
+  }
+  return new FetcherError({ message: await getText(res), status: res.status })
+}
+
+const fetcher: Fetcher = async ({
+  url,
+  method = 'GET',
+  variables,
+  query,
+  body: bodyObj,
+}) => {
+  const hasBody = Boolean(variables || bodyObj)
+  const body = hasBody
+    ? JSON.stringify(variables ? { variables } : bodyObj)
+    : undefined
+  const headers = hasBody ? { 'Content-Type': 'application/json' } : undefined
+
+  const updatedUrl = new URL(url!, process.env.NEXT_PUBLIC_API_URL)
+  updatedUrl.searchParams.set('clientId', process.env.NEXT_PUBLIC_CLIENT_ID + '')
+
+  const res = await fetch(updatedUrl.href + (query || ''), { method, body, headers })
+
+  if (res.ok) {
+    const { data } = await res.json()
+    return data
+  }
+
+  throw await getError(res)
+}
+
+export default fetcher
